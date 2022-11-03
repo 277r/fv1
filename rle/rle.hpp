9023218a6261cb0812b24351540144a294f7832c
277r <rick_degraaf@hotmail.nl>
2022-11-03 22:34:20 +0100

    removed rle/ since most of the functions in there are quite proprietary to the functions that use them, heavily relying on file IO, frame type, etc

--- rle/rle.hpp
+++
@@ -1,4 +0,0 @@
-
-
-unsigned char* rle_encode(unsigned char*inbuf, int bytes_to_encode, int &bytes_encoded_virtual, int quality);
-int rle_decode(unsigned char* outbuf,unsigned char* inbuf, int bytes_to_return, int inbuf_eof);
